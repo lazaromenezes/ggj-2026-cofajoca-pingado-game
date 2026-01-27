820887866c793099b0758e692733ad2da9cb354c
Lázaro Menezes <lazarodm@gmail.com>
2026-01-26 22:49:26 -0300

    Initial report

---
+++ game.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name Game
+
+var employee_satisfaction: float = 0.5
+var company_profits: float = 0.5
+var company_vision: float = 0.5
---
+++ suggestion.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Suggestion
+
+@export var from: String
+@export_multiline() var text: String
+@export var profit: float
+@export var expense: float
+@export var day: int
