1f5d8d872763ff2a2738e4c366eccd0cf47317e1
Lázaro Menezes <lazarodm@gmail.com>
2026-02-01 16:23:27 -0300

    Hide debug tracker

--- src/screens/office/everything_tracker.gd
+++ src/screens/office/everything_tracker.gd
@@ -13,6 +13,7 @@ var budget: int
 # End game tracker
 var negative_budget_days : int
 
+
 func _ready() -> void:
 	budget_target = budget_target_initial
 	date_manager.day_advanced.connect(_on_day_advanced)
@@ -22,12 +23,15 @@ func calculate_buget_target() -> void:
 	var modifier := lerpf(0.7, 1.3, board_satisfaction / 100.0)
 	budget_target = int(budget_target_initial * modifier)
 
+
 func check_game_over() -> void:
 	if negative_budget_days >= 2:
 		trigger_game_over()
 
+
 func trigger_game_over() -> void:
 	game_over.emit()
+
 
 func _on_day_advanced(_day: int) -> void:
 	budget = 0
@@ -35,5 +39,6 @@ func _on_day_advanced(_day: int) -> void:
 		negative_budget_days += 1
 	else:
 		negative_budget_days = 0
+		
 	check_game_over()
 	calculate_buget_target()
